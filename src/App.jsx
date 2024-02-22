817fd0f7eb1685704de923660e9106869ed89e16
fancilivain <meigangwen@gmail.com>
2024-02-22 21:19:16 +0800

    started on the css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Scroll, ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Interface } from "./components/Interface";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <ScrollControls pages={4} damping={0.1}>
+          <Experience />
+          <Scroll html>
+            <Interface />
+          </Scroll>
+        </ScrollControls>
+      </Canvas>
+    </>
   );
 }
 
