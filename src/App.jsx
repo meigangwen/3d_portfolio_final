8894fb104673b156f7da871ca3f7e9654023b815
fancilivain <meigangwen@gmail.com>
2024-02-24 18:04:25 +0800

    installed gsap

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { Scroll, ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { Interface } from "./components/Interface";
+import { ScrollManager } from "./components/ScrollManager"
+import { useState } from "react";
 
 function App() {
+  const [section, setSection] = useState(0);
+
   return (
     <>
       <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
         <color attach="background" args={["#ececec"]} />
         <ScrollControls pages={4} damping={0.1}>
+          <ScrollManager section={section} onSectionChange={setSection} />
           <Experience />
           <Scroll html>
             <Interface />
